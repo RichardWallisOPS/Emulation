3e9ac0f3b14bd3608a02c489442303f17d3538a2
Richard Wallis <richardwallis@Richards-Air.lan>
2023-01-02 17:12:13 +0000

    double buffer render

--- NES/MetalRenderer.mm
+++ NES/MetalRenderer.mm
@@ -14,8 +14,8 @@ SystemNES g_NESConsole;
 
 @implementation EmulationMetalView
 
-uint8_t keyboardPort = 0;
-uint8_t keyboardController[2] = {0 , 0};
+uint8_t m_keyboardPort = 0;
+uint8_t m_keyboardController[2] = {0 , 0};
 
 - (BOOL) acceptsFirstResponder
 {
@@ -26,35 +26,35 @@ uint8_t keyboardController[2] = {0 , 0};
 {
     if(event.keyCode == 26)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_Start;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_Start;
     }
     else if(event.keyCode == 22)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_Select;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_Select;
     }
     else if(event.keyCode == 31)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_B;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_B;
     }
     else if(event.keyCode == 35)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_A;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_A;
     }
     else if(event.keyCode == 2)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_Right;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_Right;
     }
     else if(event.keyCode == 0)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_Left;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_Left;
     }
     else if(event.keyCode == 1)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_Down;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_Down;
     }
     else if(event.keyCode == 13)
     {
-        keyboardController[keyboardPort] |= 1 << SystemNES::Controller_Up;
+        m_keyboardController[m_keyboardPort] |= 1 << SystemNES::Controller_Up;
     }
     else if(event.keyCode == 126)   // up
     {
@@ -78,9 +78,9 @@ uint8_t keyboardController[2] = {0 , 0};
     }
     else if(event.keyCode == 36) // enter
     {
-        keyboardController[keyboardPort] = 0;
-        keyboardPort = (keyboardPort + 1) % 2;
-        keyboardController[keyboardPort] = 0;
+        m_keyboardController[m_keyboardPort] = 0;
+        m_keyboardPort = (m_keyboardPort + 1) % 2;
+        m_keyboardController[m_keyboardPort] = 0;
     }
     else if(event.keyCode == 53) // esc
     {
@@ -92,35 +92,35 @@ uint8_t keyboardController[2] = {0 , 0};
 {
     if(event.keyCode == 26)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_Start);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_Start);
     }
     else if(event.keyCode == 22)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_Select);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_Select);
     }
     else if(event.keyCode == 31)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_B);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_B);
     }
     else if(event.keyCode == 35)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_A);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_A);
     }
     else if(event.keyCode == 2)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_Right);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_Right);
     }
     else if(event.keyCode == 0)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_Left);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_Left);
     }
     else if(event.keyCode == 1)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_Down);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_Down);
     }
     else if(event.keyCode == 13)
     {
-        keyboardController[keyboardPort] &= ~(1 << SystemNES::Controller_Up);
+        m_keyboardController[m_keyboardPort] &= ~(1 << SystemNES::Controller_Up);
     }
 }
 
@@ -130,11 +130,7 @@ uint8_t keyboardController[2] = {0 , 0};
 
 @property (nonatomic,readwrite) id<MTLDevice>       device;
 @property (nonatomic,readwrite) id<MTLCommandQueue> cmdQueue;
-@property (nonatomic,readwrite) id<MTLRenderPipelineState> pipelineEmulationOutputToFrameBufferTexture;
-
-@property (nonatomic,readwrite) id<MTLBuffer> emulationOutputData;
-@property (nonatomic,readwrite) id<MTLTexture> emulationOutput;
-
+@property (nonatomic,readwrite) id<MTLRenderPipelineState>  pipelineEmulationOutputToFrameBufferTexture;
 @end
 
 
@@ -146,12 +142,21 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
                                 {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
                                 {{1.f,-1.f,0.f,1.f},    {1.f,1.f}},
                                 {{1.f,1.f,0.f,1.f},     {1.f,0.f}} };
-                                
+
+size_t          m_textureId = 0;
+const size_t    m_renderTextureCount = 2;
+id<MTLTexture>  m_emulationOutput[m_renderTextureCount];
+
+- (id<MTLTexture>) nextTextureOutput
+{
+    m_textureId = (m_textureId + 1) % m_renderTextureCount;
+    return m_emulationOutput[m_textureId];
+}
+
 - (BOOL) isAppleSilicon
 {
 	return [self.device supportsFamily:MTLGPUFamilyApple1];
 }
-
 
 - (instancetype) initWithView:(EmulationMetalView*)metalView
 {
@@ -174,8 +179,11 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
         
         size_t bufferBytes = outputTextureDesc.width * outputTextureDesc.height * 4;
         
-        self.emulationOutputData = [self.device newBufferWithLength:bufferBytes options: bAppleSilicon ? MTLResourceStorageModeShared : MTLResourceStorageModeManaged];
-        self.emulationOutput = [self.emulationOutputData newTextureWithDescriptor:outputTextureDesc offset:0 bytesPerRow: outputTextureDesc.width * 4];
+        for(size_t idx = 0; idx < m_renderTextureCount; ++idx)
+        {
+            id<MTLBuffer> backingBuffer = [self.device newBufferWithLength:bufferBytes options: bAppleSilicon ? MTLResourceStorageModeShared : MTLResourceStorageModeManaged];
+            m_emulationOutput[idx] = [backingBuffer newTextureWithDescriptor:outputTextureDesc offset:0 bytesPerRow: outputTextureDesc.width * 4];
+        }
 
         self.cmdQueue = [self.device newCommandQueue];
         
@@ -205,7 +213,6 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
             
             if(arguments.count > 1)
             {
-                g_NESConsole.SetVideoOutputDataPtr((uint32_t*)self.emulationOutputData.contents);
                 if(g_NESConsole.InsertCartridge([arguments[1] cStringUsingEncoding:NSUTF8StringEncoding]))
                 {
                     g_NESConsole.PowerOn();
@@ -224,6 +231,9 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
 
 - (void) drawInMTKView:(MTKView*)view
 {
+    id<MTLTexture> currentTexture = [self nextTextureOutput];
+    id<MTLBuffer> currentTextureBacking = currentTexture.buffer;
+    
     // Input update
     {
         for(uint32_t port = 0;port < 2;++port)
@@ -232,7 +242,7 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
             uint32_t controllerBits = [PlayerControllerManager controllerBitsForNESController:port];
             
             // Mask out info and merge with keyboard
-            uint8_t portBits = (controllerBits & ControllerInfo_DataMask_08) | keyboardController[port];
+            uint8_t portBits = (controllerBits & ControllerInfo_DataMask_08) | m_keyboardController[port];
             
             // Set current controller instantious - upto game when it latches internally
             g_NESConsole.SetControllerBits(port, portBits);
@@ -241,6 +251,7 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
     
     // Emulation ticks
     {
+        g_NESConsole.SetVideoOutputDataPtr((uint32_t*)currentTextureBacking.contents);
         const size_t nNumPPUTicksPerFrame = 89342;
         for(size_t i = 0;i < nNumPPUTicksPerFrame;++i)
         {
@@ -250,9 +261,9 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
     
     // Render
     {
-        if(self.emulationOutputData.storageMode == MTLStorageModeManaged)
+        if(currentTextureBacking.storageMode == MTLStorageModeManaged)
         {
-            [self.emulationOutputData didModifyRange:NSMakeRange(0, self.emulationOutputData.length)];
+            [currentTextureBacking didModifyRange:NSMakeRange(0, currentTextureBacking.length)];
         }
         
         id<MTLCommandBuffer> cmdBuffer = [self.cmdQueue commandBuffer];
@@ -264,7 +275,7 @@ Vertex const g_quadVerts[] = {  {{-1.f,-1.f,0.f,1.f},   {0.f,1.f}},
                 [renderEncoder setFrontFacingWinding:MTLWindingCounterClockwise];
                 [renderEncoder setRenderPipelineState:self.pipelineEmulationOutputToFrameBufferTexture];
                 [renderEncoder setVertexBytes:g_quadVerts length:sizeof(Vertex) * 6 atIndex:0];
-                [renderEncoder setFragmentTexture:self.emulationOutput atIndex:0];
+                [renderEncoder setFragmentTexture:currentTexture atIndex:0];
                 [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6];
             }
             [renderEncoder endEncoding];
